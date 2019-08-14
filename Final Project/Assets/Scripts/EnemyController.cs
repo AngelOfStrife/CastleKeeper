5bd8f06d6f8573bf42bbbd8636fcb78fb636853c
Huong <huongkhong@Hotmail.co.uk>
2019-08-14 20:28:50 +0100

    New Script & Behaviours
    Spawn Enemies from Spawn Point

---
+++ Final Project/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    //public GameObject gameOverWall;
+    //public GameMasterScript gameMaster;
+
+    int hitPoint;
+
+    private void Awake()
+    {
+        hitPoint = 1;
+    }
+    
+    void FixedUpdate()
+    {
+        //check if the enemy has any hitpoints, if not destory the gameObject
+        if (hitPoint <= 0)
+        {
+            Died();
+        }
+    }
+
+    /*
+     * 
+    //Trigger condition
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.name == gameOverWall.name)
+        {
+            //game over, player loses
+            gameMaster.EndGame();
+        }
+    }
+
+    */
+
+    void Died()
+    {
+        Destroy(this.gameObject);
+    }
+}
--- Final Project/Assets/Scripts/EnemySpawn.cs
+++ Final Project/Assets/Scripts/EnemySpawn.cs
@@ -4,10 +4,16 @@ using UnityEngine;
 
 public class EnemySpawn : MonoBehaviour
 {
-
     public Transform spawnPos;
     public GameObject spawnObject;
     public bool spawnable; //check if spawn area is avaliable, do not stack spawnObjects
+
+    private void Awake()
+    {
+        spawnable = false;
+
+        
+    }
 
     // Update is called once per frame
     void Update()
